773c273e404f9bf1f527a5a564ec97e89a57c895
Roman Kozin <romanko95.rk@gmail.com>
2021-02-24 02:20:10 +0200

    Added JS Quest COre

---
+++ src/js/quest-platform.js
@@ -0,0 +1,170 @@
+// Quest Platform
+const qGen = {
+    values: {
+        step: 0,
+    },
+    defaults: {
+        image: '/src/images/placeholder.webp',
+        sfx: {
+            menu: '/src/sfx/options.mp3',
+            menuSelect: '/src/sfx/option_select.wav',
+            win: '/src/sfx/win.mp3',
+        },
+        templates: {
+            option: {
+                text: null,
+                html: '<p class="story__line__path_selector" data-step="{step}" data-value="{value}">{text}</p>'
+            }
+        },
+        elements: {
+            storyImage: document.getElementById('story__image'),
+            storyTitle: document.getElementById('story__title'),
+            storyOptions: document.getElementById('story__options'),
+        },
+        start: {
+            image: '/src/images/forest.png',
+            html: "Вы - Охотник на нечисть.<br>" +
+                "Ваш путь лежит через лес.<br>" +
+                "Перед Вами 3 пути на выбор:",
+            options: [
+                {text: 'Свернуть налево', option: 'left'},
+                {text: 'Продолжать идти прямо', option: 'straight'},
+                {text: 'Свернуть направо', option: 'right'},
+            ]
+        },
+        options: {
+            left: {
+                0: {
+                    image: null,
+                    html: "По дороге Вы наткнулись на таверну.<br>" +
+                        "Желаете посетить?",
+                    options: [
+                        {text: 'Да', option: 'left'},
+                        {text: 'Пройти мимо', option: 'right'},
+                    ],
+                },
+            },
+            right: {
+                0: {
+                    image: null,
+                    html: '',
+                    options: [
+                        {text: '', option: 'left'},
+                    ],
+                },
+            },
+            straight: {
+                0: {
+                    image: null,
+                    html: '',
+                    options: [
+                        {text: '', option: 'left'},
+                    ],
+                },
+            }
+        }
+    },
+    f: {
+        resetSteps() {
+            window._q_gen.values.step = 0;
+        },
+        increaseStep() {
+            window._q_gen.values.step++;
+        },
+        setStoryImage(image) {
+            window._q_gen
+                .defaults
+                .elements
+                .storyImage
+                .setAttribute(
+                    'style',
+                    `background-image : url('${image}');` +
+                    `background-size : cover !important;`
+                )
+        },
+        setStoryTitle(title) {
+            window._q_gen
+                .defaults
+                .elements
+                .storyTitle
+                .innerHTML = title;
+        },
+        setStoryOptionsListeners() {
+            let storyOptionsElements = document.getElementsByClassName('story__line__path_selector');
+
+            for (let index = 0; index < storyOptionsElements.length; index++) {
+                let element = storyOptionsElements[index];
+                element.addEventListener('click', function () {
+                    window._q_gen.f.playSoundOnHover('menuSelect');
+                    window._q_gen.f.updateDataByElement(element);
+                });
+
+                element.addEventListener('mouseover', function () {
+                    window._q_gen.f.playSoundOnHover('menu');
+                });
+
+            }
+        },
+        setStoryOptions(options) {
+            let htmlBlock = [],
+                template = window._q_gen.defaults.templates.option.html;
+
+            for (let index = 0; index < options.length; index++) {
+                let option = options[index];
+                htmlBlock.push(
+                    template
+                        .replace('{step}', window._q_gen.values.step)
+                        .replace('{text}', option.text ?? option.html)
+                        .replace('{value}', option.option)
+                );
+            }
+
+            window
+                ._q_gen
+                .defaults
+                .elements
+                .storyOptions
+                .innerHTML = htmlBlock.join('');
+
+            window._q_gen.f.setStoryOptionsListeners();
+        },
+        playSoundOnHover(sound) {
+            let audio = new Audio(window._q_gen.defaults.sfx[sound]);
+            audio.play();
+        },
+        updateDataByElement(element) {
+            let step = element.getAttribute('data-step'),
+                option = element.getAttribute('data-value');
+
+            let story = window._q_gen.defaults.options[option][step],
+                restart = story['restart'] ?? false;
+
+            if (restart) {
+                window._q_gen.f.resetSteps();
+                window._q_gen.init();
+
+                return;
+            }
+
+            window._q_gen.f.increaseStep();
+            window._q_gen.f.setStoryImage(story.image ?? window._q_gen.defaults.image);
+            window._q_gen.f.setStoryTitle(story.html);
+            window._q_gen.f.setStoryOptions(story.options);
+        }
+    },
+    init() {
+        let start = window._q_gen.defaults.start;
+        window._q_gen.f.setStoryImage(start.image ?? window._q_gen.defaults.image);
+        window._q_gen.f.setStoryTitle(start.html);
+        window._q_gen.f.setStoryOptions(start.options);
+    }
+};
+
+// Global access assign
+window._q_gen = qGen;
+
+if (!window._q_gen.hasOwnProperty('init')) {
+    throw new Error('Failed to initialize Q Gen');
+}
+
+window._q_gen.init();
